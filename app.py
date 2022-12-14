e4593137bcdb7fda02d0459331a184db37567356
Sanjay Kumar <74352452+1242sanjay@users.noreply.github.com>
2022-12-14 07:02:13 +0000

    homepage and projects section

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-  return "hello world"
+  return render_template('home.html')
 
+@app.route('/projects')
+def projects():
+  return render_template('projects.html')
 
 
 if __name__ == '__main__':
